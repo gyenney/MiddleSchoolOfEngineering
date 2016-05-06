c20fa4c4df7b43155484f94d97bc5a9fb0dc157f
gyenney <gregory.yenney@gmail.com>
2016-05-05 22:58:14 -0700

    UPdates.

---
+++ jaysomdahl1/First.py
@@ -0,0 +1,16 @@
+print("Welcome to the Guessing Game")
+
+g = input("Guess the number: ")
+
+guess = int(g)
+
+if guess == 5:
+
+    	print("You win!")
+
+else:
+
+    	print("You lose!")
+
+
+print("Game over!")
